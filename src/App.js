81005c86b46d487beb703aa040bfa1c28fb367a1
amcc <alistairmcclymont@gmail.com>
2021-12-07 18:56:00 +0000

    feat: change to simple astronaut

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import "@google/model-viewer";
 function App() {
   return (
     <div className="App">
+      {/* 
+      This is a very big slow model
       <model-viewer
         alt="Neil Armstrong's Spacesuit from the Smithsonian Digitization Programs Office and National Air and Space Museum"
         src="/models/NeilArmstrong.glb"
@@ -15,6 +17,18 @@ function App() {
         seamless-poster
         shadow-intensity="1"
         camera-controls
+      ></model-viewer> */}
+
+      <model-viewer
+        src="/models/Astronaut.glb"
+        ios-src=""
+        poster="/models/astronaut.png"
+        alt="A 3D model of an astronaut"
+        shadow-intensity="1"
+        camera-controls
+        auto-rotate
+        ar
+        ar-modes="webxr scene-viewer quick-look"
       ></model-viewer>
     </div>
   );
